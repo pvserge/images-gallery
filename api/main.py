2c7baf0e9ccf0d6ba11bb38fe4b204421bba43d4
Sergey Perepelov <pvserge1@gmail.com>
2025-05-25 15:34:05 +0300

    basic images endpoint with get and post methods

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
+"""Main module"""
+
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -23,12 +29,31 @@ app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
+    """The endpoint to get an image from unsplash api"""
+
     word = request.args.get("quary")
     headers = {"Accept-Version": "v1", "Authorization": f"Client-ID {UNSPLASH_KEY}"}
     params = {"quary": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    """The endpoint to get images from database or save one to the datebase"""
+
+    if request.method == "GET":
+        # read images from database
+        list_images = images_collection.find({})
+        return jsonify([img for img in list_images])
+    if request.method == "POST":
+        # save image to the database
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 
 if __name__ == "__main__":
