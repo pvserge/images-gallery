040c57b2bd0c1dbd81c159107ab18324295a7cb2
Sergey Perepelov <pvserge1@gmail.com>
2025-05-26 09:00:06 +0300

    single image endpoint with delete method enabled

--- api/main.py
+++ api/main.py
@@ -58,5 +58,19 @@ def images():
         return {"inserted_id": inserted_id}
 
 
+@app.route("/images/<image_id>", methods=["DELETE"])
+def image(image_id):
+    """The endpoint for delete image with appropriate id from the database"""
+
+    if request.method == "DELETE":
+        # delete image from database
+        result = images_collection.delete_one({"_id": image_id})
+        if not result:
+            return {"error", "Image was not deleted. Please try again"}, 500
+        if result and not result.deleted_count:
+            return {"error": "Image not found"}, 404
+        return {"deleted_id": image_id}
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
